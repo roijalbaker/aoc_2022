47e5ba3e1f0e7d8fce0e645c25557361d9f2bbbc
Tim de Rooij <tim.derooij@mindfoundry.ai>
2022-12-02 09:26:17 +0000

    day2 part2

--- day2.py
+++ day2.py
@@ -1,7 +1,7 @@
 shapes = {
-    "rock": {"win": "scissors", "score": 1},
-    "paper": {"win": "rock", "score": 2},
-    "scissors": {"win": "paper", "score": 3},
+    "rock": {"win": "scissors", "lose": "paper", "score": 1},
+    "paper": {"win": "rock", "lose": "scissors", "score": 2},
+    "scissors": {"win": "paper", "lose": "rock", "score": 3},
 }
 
 other = {
@@ -16,8 +16,15 @@ me = {
     "Z": "scissors"
 }
 
+me2 = {
+    "X": "lose",
+    "Y": "draw",
+    "Z": "win"
+}
+
 if __name__ == "__main__":
     score = 0
+    score2 = 0
     with open("day2_input1.txt") as f:
         for line in f.readlines():
             you, i = line[:3].split(" ")
@@ -29,4 +36,14 @@ if __name__ == "__main__":
                 # win
                 score += 6
             # loose += 0
+
+            other_shape = shapes[other[you]]
+            if me2[i] == "draw":
+                score2 += 3 + other_shape["score"]
+            elif me2[i] == "win":
+                score2 += 6 + shapes[other_shape["lose"]]["score"]
+            elif me2[i] == "lose":
+                score2 += 0 + shapes[other_shape["win"]]["score"]
+
     print(score)
+    print(score2)
