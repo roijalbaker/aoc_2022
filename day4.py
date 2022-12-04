5f7da0453aa3bd86a4b2e71b4b0333b14254f066
Tim de Rooij <tim.derooij@mindfoundry.ai>
2022-12-04 10:44:12 +0000

    day 4 part 2

--- day4.py
+++ day4.py
@@ -8,13 +8,16 @@ def to_ids(string):
     return set(range(int(first), int(last)+1))
 
 
-def intersect(left, right):
+def intersect(left, right, part):
     left_set = to_ids(left)
     right_set = to_ids(right)
-    return left_set.issubset(right_set) or right_set.issubset(left_set)
+    if part == 1:
+        return left_set.issubset(right_set) or right_set.issubset(left_set)
+    return len(right_set.intersection(left_set)) > 0
 
 
 if __name__ == "__main__":
     assignments = parse_file("day4_input1.txt")
 
-    print(sum([intersect(*assignment) for assignment in assignments]))
+    print(sum([intersect(*assignment, part=1) for assignment in assignments]))
+    print(sum([intersect(*assignment, part=2) for assignment in assignments]))
