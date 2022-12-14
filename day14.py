d31814bb00a0c3a033f7069ae20c9e1c3d0cc21f
Tim de Rooij <tim.derooij@mindfoundry.ai>
2022-12-14 20:43:49 +0000

    day 14 part 1

--- day14.py
+++ day14.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def line_to_path(line: str):
-    path = r"(\d+,\d)"
+    path = r"(\d+,\d+)"
     points = re.findall(path, line)
     points = [p.split(",") for p in points]
     points = [(int(a), int(b)) for a, b in points]
@@ -22,31 +22,31 @@ def rocks_to_map(rocks):
     y_max, x_max = np.array(rocks).max(axis=1).max(axis=0)
     blocked = np.array([[False] * (y_max+1)] * (x_max+1))
     for (start_x, start_y), (end_x, end_y) in rocks:
-        if start_x == end_x:
+        if (x := start_x) == end_x:
             ys = range(start_y, end_y+1, 1) if end_y > start_y else range(end_y, start_y+1, 1)
             for y in ys:
-                blocked[y, start_x] = True
-        elif start_y == end_y:
+                blocked[y, x] = True
+        elif (y := start_y) == end_y:
             xs = range(start_x, end_x+1, 1) if end_x > start_x else range(end_x, start_x+1, 1)
             for x in xs:
-                blocked[start_y, x] = True
+                blocked[y, x] = True
         else:
             raise ValueError()
     return blocked
 
 
 if __name__ == "__main__":
-    rocks = parse_file("day14_test1.txt")
+    rocks = parse_file("day14_input.txt")
     rocks = rocks_to_map(rocks)
     np.savetxt("rocks.txt", rocks, fmt="%d")
-    
+
     start = rocks.sum()
 
     landed = True
     while landed:
         x = 500
         for y in range(rocks.shape[0]):
-            
+
             if y+1 == rocks.shape[0]:
                 landed = False
                 break
