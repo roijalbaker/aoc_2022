912710f0110ddda6f65cf3903bb5827afabab774
Tim de Rooij <tim.derooij@mindfoundry.ai>
2022-12-16 08:38:39 +0000

    day 15 less naive

--- day15.py
+++ day15.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 from pydantic.dataclasses import dataclass
 from numpy.linalg import norm
+from typing import List
 
 
 def line_to_path(line: str):
@@ -43,6 +44,34 @@ class Sensor(Beacon):
         return norm(self.as_vector - np.array([x, y]), 1) <= self.distance
 
 
+def solve_less_naive(sensors: List[Sensor], y):
+    covered_x = set([])
+    for s in sensors:
+        dist_y = int(s.distance - norm(np.array([s.x, y]) - s.as_vector))
+        if dist_y > 0:
+            covered_x.update(set(range(s.x-dist_y, s.x+dist_y+1,1)))
+    for s in sensors:
+        if s.beacon.y == y and s.beacon.x in covered_x:
+            covered_x.remove(s.beacon.x)
+    print(len(covered_x))
+    print(covered_x)
+
+
+def solve_naive(sensors: List[Sensor], y, x_min, x_max):
+    outside_range = []
+    xrange = range(x_min-2, x_max + 2, 1)
+    existing_beacons = [s.beacon.x for s in sensors if s.beacon.y == y]
+    for x in xrange:
+        if x in existing_beacons:
+            outside_range.append(True)  # hack to evade beacons
+            continue
+        # loop over all sensors to figure out whether it is outside all of these ranges
+        outside_all_ranges = next((not value for s in sensors if (value := s.inside_range(x, y))), True)
+        outside_range.append(outside_all_ranges)
+        # print(x,y, outside_range[-1])
+    print(sum([not x for x in outside_range]))
+
+
 if __name__ == "__main__":
     sensors_beacons = parse_file("day15_test1.txt")
     sensors = [Sensor(s_x, s_y, Beacon(b_x, b_y)) for s_x, s_y, b_x, b_y in sensors_beacons]
@@ -50,19 +79,6 @@ if __name__ == "__main__":
     x_min = min([min(sensor.x, sensor.beacon.x) for sensor in sensors])
     y_max = max([max(sensor.y, sensor.beacon.y) for sensor in sensors])
     y_min = min([min(sensor.y, sensor.beacon.y) for sensor in sensors])
-
-    y = 10
-    outside_range = []
-    xrange = range(x_min-2, x_max + 2, 1)
-    existing_beacons = [s.beacon for s in sensors if s.beacon.y == y]
-    for x in xrange:
-        if x in [b.x for b in existing_beacons]:
-            outside_range.append(True)  # hack to evade beacons
-            continue    
-        # loop over all sensors to figure out whether it is outside all of these ranges
-        outside_all_ranges = next((not value for s in sensors if (value := s.inside_range(x, y))), True)
-        outside_range.append(outside_all_ranges)
-        # print(x,y, outside_range[-1])
-    print(list(zip(xrange, outside_range)))
-    print(sum([not x for x in outside_range]))
-
+    print(x_min, x_max, y_min, y_max)
+    solve_naive(sensors, 10, x_min, x_max)  # takes ages
+    solve_less_naive(sensors, 10)
