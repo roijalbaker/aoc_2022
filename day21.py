cf5c275a0c3defcbe6ac63b71d50b30c958e6263
Tim de Rooij <tim.derooij@mindfoundry.ai>
2022-12-21 17:21:17 +0100

    day 21 part 2

--- day21.py
+++ day21.py
@@ -1,39 +1,51 @@
 from typing import Dict
+from sympy import parse_expr, solve
 
 
 class Monkey:
-    def __init__(self, name: str, value: str):
+    def __init__(self, name: str, value: str, part: str = "a"):
         self.name = name.strip()
         self.init_value = value.strip()
         self._value = None
+        self.part = part
 
-    def value(self, monkeys: Dict[str, "Monkey"]):
+    def value(self, monkeys: Dict[str, "Monkey"]):   
         if self._value is None:
             try:
-                self._value = int(self.init_value.strip())
+                if self.part == "b" and self.name == "humn":
+                    self._value = "x"
+                else:
+                    self._value = int(self.init_value.strip())
             except Exception:
                 left, *_, right = self.init_value.split(" ")
                 left = monkeys[left]
                 right = monkeys[right]
                 calculate = (
-                    self.init_value
+                    (self.init_value.replace("+", "-") if self.part == "b" and self.name=="root" else self.init_value)
                     .replace(left.name, f"{left.value(monkeys)}")
                     .replace(right.name, f"{right.value(monkeys)}")
                 )
-                self._value = eval(calculate)
+                if "x" in calculate:
+                    self._value = f"({calculate})"
+                else:
+                    self._value = int(eval(calculate))
 
         return self._value
 
 
-def parse_file(fn) -> Dict[str, Monkey]:
+def parse_file(fn, part: str = "a") -> Dict[str, Monkey]:
     monkeys = {}
     with open(fn, "r") as f:
         for line in f.readlines():
-            monkey = Monkey(*line.split(":"))
+            monkey = Monkey(*line.split(":"), part=part)
             monkeys.update({monkey.name: monkey})
     return monkeys
 
 
 if __name__ == "__main__":
-    monkeys = parse_file("day21_input1.txt")
+    monkeys = parse_file("day21_input1.txt", "a")
     print(monkeys["root"].value(monkeys))
+
+    monkeys = parse_file("day21_input1.txt", "b")
+    sympy_eq = parse_expr(monkeys["root"].value(monkeys)[1:-1])
+    print(solve(sympy_eq)[0])
